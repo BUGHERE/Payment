f0b3528012781c20cb302df640aef904f14559ce
1911481-Wang Yunhao <1595014015@qq.com>
2022-06-02 02:23:26 +0800

    add valueobject package & add R object & test R

--- src/main/java/com/cozyy/payment/controller/ProductController.java
+++ src/main/java/com/cozyy/payment/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.cozyy.payment.controller;
 
+import com.cozyy.payment.valueobject.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @Api(tags = "商品管理")  // Swagger2 ui
 @RestController  // 保证以json格式返回
@@ -12,11 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
     @ApiOperation("测试接口")  // Swagger2 ui
     @GetMapping("/test")
-    public String test() {
+    public R test() {
         /***
          * test for web request
          * visited by http://localhost:8090/api/product/test
          */
-        return "test";
+        return R.ok();
+    }
+    @ApiOperation("/addTime func")
+    @GetMapping("/addTime")
+    public R addTime() {
+        /**
+         * add time message to R object and return R
+         */
+        return R.ok().data("time", new Date());
     }
 }
